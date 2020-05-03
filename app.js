d5a104d56cb230e3d85ec13a4c9d250a6e141d7f
dfnk5516 <rayshoo@naver.com>
2020-05-03 13:27:30 +0900

    commit

--- app.js
+++ app.js
@@ -18,15 +18,34 @@ app.use(express.urlencoded({extended:false}));
 
 /* 라우터 */
 app.use('/', express.static(path.join(__dirname, './public')));
+app.use('/uploads', express.static(path.join(__dirname, './storages'))); // 정적폴더 > 보안을 위해 실제 경로와 다르게 설정
 
-app.get('/pug', (req, res, next)=>{
-    const user = [
-        {id : 1, name : '홍길동'},
-        {id : 2, name : '홍길순'},
-        {id : 3, name : '홍길만'}
-    ];
-    const title = 'PUG로 만든 페이지 입니다';
+const navis = [
+  {id : 1, title : 'About', link:'/about'},
+  {id : 2, title : 'Board', link:'/board'},
+  {id : 3, title : 'Gallery', link:'/gallery'}
+];
+app.get('/', (req, res, next)=>{
+    
+    const title = 'index.html';
     const headTitle = 'PUG 페이지';
-
-    res.render('index', {user, title, headTitle}); //ES6 문법으로 이름 같아서 자동으로 map처럼 전달됨 {user : user, title : title, ...}
+    res.render('index', {navis, title, headTitle}); //ES6 문법으로 이름 같아서 자동으로 map처럼 전달됨 {user : user, title : title, ...}
+});
+app.get('/about', (req, res, next)=>{
+    
+    const title = 'about.html';
+    const headTitle = 'PUG 페이지';
+    res.render('about', {navis, title, headTitle}); //ES6 문법으로 이름 같아서 자동으로 map처럼 전달됨 {user : user, title : title, ...}
+});
+app.get('/board', (req, res, next)=>{
+    
+    const title = 'board.html';
+    const headTitle = 'PUG 페이지';
+    res.render('board', {navis, title, headTitle}); //ES6 문법으로 이름 같아서 자동으로 map처럼 전달됨 {user : user, title : title, ...}
+});
+app.get('/gallery', (req, res, next)=>{
+    
+    const title = 'gallery.html';
+    const headTitle = 'PUG 페이지';
+    res.render('gallery', {navis, title, headTitle}); //ES6 문법으로 이름 같아서 자동으로 map처럼 전달됨 {user : user, title : title, ...}
 });
